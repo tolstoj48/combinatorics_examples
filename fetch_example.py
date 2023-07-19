543bac24f7ee4b372cc2f6c8ae75dcbd9282c4c2
Petr Musil <petr0musil@gmail.com>
2023-07-19 11:21:57 +0200

    Added category and example pick up resolution.

--- fetch_example.py
+++ fetch_example.py
@@ -2,21 +2,29 @@
 # přidat úlohy z http://kombinatorika.rubesz.cz/
 
 import random
+import sys
 import data
 
 
 class Example:
-    def __init__(self):
-        # TODO: upravit na slovníky - slovníky z důvodu ozdrojování
-        self.category_number = random.randint(0, len(data.examples) - 1)
+    def __init__(self, start=0, end=len(data.examples) - 1):
+        self.start = start
+        self.end = end
+        self.category_number = random.randint(self.start, self.end)
         self.number = random.randint(
-            1, len(data.examples[self.category_number]) - 1)
+            0, len(data.examples[self.category_number]) - 1)
         if self.category_number == 0:
             self.category = "Variace bez opakování"
         elif self.category_number == 1:
             self.category = "Permutace bez opakování"
         elif self.category_number == 2:
             self.category = "Kombinace bez opakování"
+        elif self.category_number == 3:
+            self.category = "Variace s opakováním"
+        elif self.category_number == 4:
+            self.category = "Permutace s opakováním"
+        elif self.category_number == 5:
+            self.category = "Kombinace s opakováním"
 
     def fetch_example(self):
         print("---------------------------------")
@@ -25,18 +33,25 @@ class Example:
         vysledek = "N"
         while vysledek.lower() != "a":
             vysledek = input(
-                "Chcete vidět výsledek, případně s řešením? A pro ano, N pro ne.")
+                "Chcete vidět výsledek, případně s řešením? \"A\\a\" pro ano, \"N\\n\" pro ne.")
         print("---------------------------------")
         print(
             f"Výsledek je roven: {data.examples[self.category_number][self.number][1]}")
         print("*****")
         print(f"A jde o: {self.category}")
         print("*****")
-        print(f"Šlo o příklad číslo: {self.number} a zdroj je: {data.examples[self.category_number][0]}")
+        print(f"Šlo o příklad číslo: {self.number} a zdroj je: {data.sources[data.examples[self.category_number][self.number][2]]}")
         print("---------------------------------")
         return
 
 
 if __name__ == "__main__":
-    example = Example()
-    example.fetch_example()
+    try:
+        if(sys.argv[2] == "no-repeat"):
+            example = Example(start = 0, end = 2)
+        elif(sys.argv[2] == "repeat"):
+            example = Example(start = 3, end = 5)
+        example.fetch_example()
+    except:
+        example = Example()
+        example.fetch_example()
